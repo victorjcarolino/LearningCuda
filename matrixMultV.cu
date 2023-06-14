ea42eea2bd518e077abd348643d6d37fc7d13d67
Victor-Carolino <vjc225@lehigh.edu>
2023-06-14 10:39:16 -0400

    Added skeleton code for matrixMult kernel

--- matrixMultV.cu
+++ matrixMultV.cu
@@ -6,6 +6,19 @@
 #include <fstream>
 #include <string>
 
+__device__ void MatrixMultiplication(float* M, float* N, float* P, int Width) {
+	int size = Width * Width * sizeof(float);
+	float* Md, Nd, Pd;
+	
+	// Allocate device memory for M, N, and P
+	// copy M and N to allocated device memory locations
+
+	// Kernel invocation code - to have the device to perform the actual matrix multiplication
+
+	// copy P from the device memory
+	// Free device matrices
+}
+
 int main(void) {
 	// Allocate and initialize the matrices M, N, P
 	// I/O to read the input matrices M and N
@@ -14,3 +27,6 @@ int main(void) {
 	
 	// I/O to write the output matrix P
 	// Free matrices M, N, P
+
+	return 0;
+}
